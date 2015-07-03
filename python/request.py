87bc562768a0bad78fce7b77b6ca2afbb8bd077e
Pierre-Marie Dartus <pierre.dartus@salesforce.com>
2015-07-03 08:49:35 -0700

    Make simple request python work

--- python/request.py
+++ python/request.py
@@ -1,41 +1,36 @@
+#!/usr/bin/env python3
+
 import json
 import os
 import requests
 import statistics
 from datetime import datetime
+from utils import Config, gather_metrics
 
-CONFIG_PATH = os.getcwd() + '/config.json'
-with open(CONFIG_PATH) as data_file:    
-    config = json.load(data_file)
+store_path = os.getcwd() + '/stores.json'
+config = Config(store_path)
 
-def gather_metrics(times):
-	return {
-		"max": max(times),
-		"min": min(times),
-		"avg": statistics.mean(times)
-	}
-
-def run_single_iter():
+def run_single_iter(url):
 	session = requests.session()
 	start = datetime.now()
-	
-	for path in config['paths']:
-		url = 'https://' + config['host'] + path; 
-		r = session.get(url, verify=True)
-		if(r.status_code != 200):
-			print(r.status_code)
+	r = session.get(url, verify=True)
 
-	end = datetime.now()
-	diff = (end - start).microseconds / 1000
+	if(r.status_code != 200):
+		print(r.status_code)
+	diff = datetime.now() - start
 	return diff
 	
 
 def run():
 	exec_time = []
-	for ite in range(config['iteration']):
-		exec_time.append(run_single_iter())
+	start = datetime.now()
+	
+	for store in config.stores:
+		time = run_single_iter(store['url'])
+		exec_time.append(time)
 
-	print(gather_metrics(exec_time))
+	total_time = datetime.now() - start
+	print(gather_metrics(exec_time, total_time))
 
 if __name__ == '__main__':
 	run()
--- python/utils.py
+++ python/utils.py
@@ -4,13 +4,35 @@ import requests
 import statistics
 from datetime import datetime
 
-CONFIG_PATH = os.getcwd() + '/config.json'
-with open(CONFIG_PATH) as data_file:    
-    config = json.load(data_file)
 
-def gather_metrics(times):
+def gather_metrics(times, total_time):
+	times = [time.microseconds / 1000 for time in times]
 	return {
+		"requestsNumber": len(times),
+		"totalTimePerSec": total_time.seconds,
+		"numberRequestPerSec": len(times) / total_time.seconds,
 		"max": max(times),
 		"min": min(times),
 		"avg": statistics.mean(times)
 	}
+
+class Config(object):
+	def __init__(self, store_path):
+		config_path = os.getcwd() + '/config.json'
+		with open(config_path) as data_file:
+			config = json.load(data_file)
+			self.concurrency = config['concurrency']
+			self.iterations = config['iterations']
+
+		stores = []
+		with open(store_path) as data_file:    
+			for line in data_file:
+				stores.append(json.loads(line))
+
+		if self.iterations == None:
+			self.iterations = len(stores)
+		stores = stores[:self.iterations]
+
+		for store in stores:
+			store['url'] = "http://www.searsoutlet.com/d/store.jsp?store={}&cid=0&ps=50".format(store['store_id']);
+		self.stores = stores
